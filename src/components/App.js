8ae304f22d364b2d115f7038f770b272f73f231d
Tyler Lai <lai.t.tyler@gmail.com>
2020-05-06 22:41:42 -0500

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
+
+console.log(process.env.REACT_APP_YOUTUBE_API_KEY);
+
+class App extends Component {
+  state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit('lofi');
+  }
+
+  onTermSubmit = async (term) => {
+    const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+    const response = await youtube.get('/search', {
+      params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: API_KEY,
+        q: term,
+      },
+    });
+
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({
+      selectedVideo: video,
+    });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
